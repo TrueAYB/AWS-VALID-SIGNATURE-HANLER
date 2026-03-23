1d391831a425adfc918c359439e307bcf96c5c28
TrueAYBYouTube <ayuubajmal10@outlook.com>
2026-03-23 14:02:58 +0000

    Implementing a AWS Lamdba typing package to the function

--- Valid Handler Signature.py
+++ Valid Handler Signature.py
@@ -1,3 +1,5 @@
+from aws_lambda_typing.events import S3Event
+from aws_lambda_typing.context import Context
 from typing import Dict, Any
-      
-def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
+
+def lambda_handler(event: S3Event, context: Context) -> Dict[str, Any]:
